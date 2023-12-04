6cc11206a470f3d1d7fd26dca41f027e19ea713e
AGorelski <132019892+AGorelski@users.noreply.github.com>
2023-12-04 16:33:18 +0200

    Added initial home layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import Carousel from "./components/carousel/Carousel"
+import CategoriesStart from "./components/categories/CategoriesStart"
+import FeaturedStart from "./components/featured/FeaturedStart"
+import Footer from "./components/footer/Footer"
+import NavBar from "./components/navbar/NavBar"
+import Offer from "./components/offer/Offer"
+import ProductsStart from "./components/products/ProductsStart"
+import Topbar from "./components/topbar/Topbar"
+
 function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <Topbar />
+      <NavBar />
+      <Carousel />
+      <FeaturedStart />
+      <CategoriesStart />
+      <ProductsStart />
+      <Offer />
+      <Footer />
     </>
   )
 }
